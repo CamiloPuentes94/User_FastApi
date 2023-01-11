45c7ed92ceb2c83adf943fe8a26bd907a29e0c55
Juan Puentes <jcpsandoval94@gmail.com>
2023-01-11 13:06:12 -0500

    Se agrega conexion a mongo db atlas

--- db/client.py
+++ db/client.py
@@ -1,3 +1,7 @@
 from pymongo import MongoClient
 
-db_client = MongoClient()
+# Base de datos local
+# db_client = MongoClient().local
+
+# Base de datos cloud
+db_client = MongoClient("mongodb+srv://Camilo:Samimajo10@cluster0.n37cpaj.mongodb.net/?retryWrites=true&w=majority").test
